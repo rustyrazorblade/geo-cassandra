bc0e76322e4c67038aead902dd83c6f7e3d3f94e
Jon Haddad <jon@jonhaddad.com>
2017-12-13 16:51:12 -0500

    adding in argument parsing, yay

--- src/main/kotlin/com/rustyrazorblade/geocassandra/Main.kt
+++ src/main/kotlin/com/rustyrazorblade/geocassandra/Main.kt
@@ -1,5 +1,18 @@
 package com.rustyrazorblade.geocassandra
+import com.beust.jcommander.JCommander
+import com.beust.jcommander.Parameter
+
+class Args {
+    @Parameter(names = arrayOf("-c", "--cluster"),
+               description = "Cassandra cluster seeds (comma separated for multiple)",
+               required = true)
+    var cluster = String()
+}
 
 fun main(args: Array<String>) {
+    var parsed = JCommander.newBuilder().addObject(Args()).build().parse(*args)
+
+
     println("Starting up the fun")
+
 }
