44bcc7d14f9c69604f5c3aa874da05224fb13515
Jon Haddad <jon@jonhaddad.com>
2017-12-13 22:07:10 -0500

    devices are good

---
+++ src/main/kotlin/com/rustyrazorblade/geocassandra/Device.kt
@@ -0,0 +1,12 @@
+package com.rustyrazorblade.geocassandra
+
+/*
+
+CREATE TABLE device (device text primary key, bloom_filter int)
+ WITH compaction = {'class':'LeveledCompactionStrategy'}
+ AND caching = {'keys': 'ALL', 'rows_per_partition': 'ALL'};
+
+*/
+class Device {
+
+}
--- src/main/kotlin/com/rustyrazorblade/geocassandra/User.kt
+++
@@ -1,5 +0,0 @@
-package com.rustyrazorblade.geocassandra
-
-class User {
-
-}
